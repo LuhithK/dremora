bfe4825aaeac0e53c0fa6d15e298c0476afe8f8b
LuhithK <121594327+LuhithK@users.noreply.github.com>
2025-08-24 16:50:35 +0530

    Remove duplicate TravelersDestinations import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import Hero from '../components/Hero';
 import WhyChooseUs from '../components/WhyChooseUs';
 import QuotationForm from '../components/QuotationForm';
 import TravelersDestinations from '../components/TravelersDestinations';
-import TravelersDestinations from '../components/TravelersDestinations';
 import FeaturedPackages from '../components/FeaturedPackages';
 import Services from '../components/Services';
 import Testimonials from '../components/Testimonials';
