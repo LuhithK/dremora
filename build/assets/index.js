51303b3d7a2ea2b30d2fb481d236cd5655bb349f
LuhithK <121594327+LuhithK@users.noreply.github.com>
2025-09-22 11:44:33 +0530

    final5

--- build/assets/index.js
+++ build/assets/index.js
@@ -1,50 +1,50 @@
-// Asset imports for easy management
+// Asset URLs for public folder
 // Hero Images
-export { default as HeroBg1 } from './images/hero/hero-bg-1.jpg';
-export { default as HeroBg2 } from './images/hero/hero-bg-2.jpg';
+export const HeroBg1 = '/images/hero/hero-bg-1.jpg';
+export const HeroBg2 = '/images/hero/hero-bg-2.jpg';
 // Package Images
-export { default as CulturalTriangle } from './images/packages/cultural-triangle.jpg';
-export { default as HillCountry } from './images/packages/hill-country.jpg';
-export { default as SouthernCoast } from './images/packages/southern-coast.jpg';
-export { default as WildlifeSafari } from './images/packages/wildlife-safari.jpg';
-export { default as AdventureTour } from './images/packages/adventure-tour.jpg';
-export { default as HoneymoonSpecial } from './images/packages/honeymoon-special.jpg';
+export const CulturalTriangle = '/images/packages/cultural-triangle.jpg';
+export const HillCountry = '/images/packages/hill-country.jpg';
+export const SouthernCoast = '/images/packages/southern-coast.jpg';
+export const WildlifeSafari = '/images/packages/wildlife-safari.jpg';
+export const AdventureTour = '/images/packages/adventure-tour.jpg';
+export const HoneymoonSpecial = '/images/packages/honeymoon-special.jpg';
 // Destination Images
-export { default as Kandy } from './images/destinations/kandy.jpg';
-export { default as Galle } from './images/destinations/galle.jpg';
-export { default as Ella } from './images/destinations/ella.jpg';
-export { default as Sigiriya } from './images/destinations/sigiriya.jpg';
-export { default as NuwaraEliya } from './images/destinations/nuwara-eliya.jpg';
-export { default as YalaNationalPark } from './images/destinations/yala-national-park.jpg';
-export { default as Mirissa } from './images/destinations/mirissa.jpg';
-export { default as AdamPeak } from './images/destinations/adam-peak.jpg';
+export const Kandy = '/images/destinations/kandy.jpg';
+export const Galle = '/images/destinations/galle.jpg';
+export const Ella = '/images/destinations/ella.jpg';
+export const Sigiriya = '/images/destinations/sigiriya.jpg';
+export const NuwaraEliya = '/images/destinations/nuwara-eliya.jpg';
+export const YalaNationalPark = '/images/destinations/yala-national-park.jpg';
+export const Mirissa = '/images/destinations/mirissa.jpg';
+export const AdamPeak = '/images/destinations/adam-peak.jpg';
 // Team Images
-export { default as CEO } from './images/team/ceo.jpg';
-export { default as OperationsHead } from './images/team/operations-head.jpg';
-export { default as TourGuide } from './images/team/tour-guide.jpg';
+export const CEO = '/images/team/ceo.jpg';
+export const OperationsHead = '/images/team/operations-head.jpg';
+export const TourGuide = '/images/team/tour-guide.jpg';
 // Testimonial Images
-export { default as Customer1 } from './images/testimonials/customer-1.jpg';
-export { default as Customer2 } from './images/testimonials/customer-2.jpg';
-export { default as Customer3 } from './images/testimonials/customer-3.jpg';
-export { default as Customer4 } from './images/testimonials/customer-4.jpg';
+export const Customer1 = '/images/testimonials/customer-1.jpg';
+export const Customer2 = '/images/testimonials/customer-2.jpg';
+export const Customer3 = '/images/testimonials/customer-3.jpg';
+export const Customer4 = '/images/testimonials/customer-4.jpg';
 // Logo Images
-export { default as DremoraLogo } from './images/logo/dremora-logo.png';
-export { default as DremoraLogoWhite } from './images/logo/dremora-logo-white.png';
+export const DremoraLogo = '/images/logo/dremora-logo.png';
+export const DremoraLogoWhite = '/images/logo/dremora-logo-white.png';
 // About Images
-export { default as CompanyStory } from './images/about/company-story.jpg';
-export { default as TeaPlantation } from './images/about/tea-plantation.jpg';
+export const CompanyStory = '/images/about/company-story.jpg';
+export const TeaPlantation = '/images/about/tea-plantation.jpg';
 // Gallery Images
-export { default as Gallery1 } from './images/gallery/gallery-1.jpg';
-export { default as Gallery2 } from './images/gallery/gallery-2.jpg';
-export { default as Gallery3 } from './images/gallery/gallery-3.jpg';
-export { default as Gallery4 } from './images/gallery/gallery-4.jpg';
-export { default as Gallery5 } from './images/gallery/gallery-5.jpg';
-export { default as Gallery6 } from './images/gallery/gallery-6.jpg';
+export const Gallery1 = '/images/gallery/gallery-1.jpg';
+export const Gallery2 = '/images/gallery/gallery-2.jpg';
+export const Gallery3 = '/images/gallery/gallery-3.jpg';
+export const Gallery4 = '/images/gallery/gallery-4.jpg';
+export const Gallery5 = '/images/gallery/gallery-5.jpg';
+export const Gallery6 = '/images/gallery/gallery-6.jpg';
 // Videos
-export { default as SriLankaIntro } from './videos/sri-lanka-intro.mp4';
-export { default as CulturalHeritage } from './videos/cultural-heritage.mp4';
-export { default as WildlifeSafariVideo } from './videos/wildlife-safari.mp4';
-// Asset URLs (for external images currently being used)
+export const SriLankaIntro = '/videos/sri-lanka-intro.mp4';
+export const CulturalHeritage = '/videos/cultural-heritage.mp4';
+export const WildlifeSafariVideo = '/videos/wildlife-safari.mp4';
+// External Asset URLs (for fallback or external use)
 export const ASSET_URLS = {
     hero: {
         bg1: 'https://images.pexels.com/photos/1450360/pexels-photo-1450360.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&fit=crop',
